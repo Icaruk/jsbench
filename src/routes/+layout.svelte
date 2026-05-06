b771e15a49b742c257b98852631bb4d3716bbfac
Icaruk <adri-rk@hotmail.com>
2026-05-07 00:25:35 +0200

    feat: add animate favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import faviconRunning from '$lib/assets/favicon-running.svg';
+	import { state as bench } from '$lib/state.svelte.js';
 
 	let { children } = $props();
+
+	let iconHref = $derived(bench.running ? faviconRunning : favicon);
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={iconHref} /></svelte:head>
 {@render children()}
